292302bcd48bace991c35faf7c1c1c0d7f45ae85
Jamshid Niroomand <jmsnrmnd@gmail.com>
2015-07-30 11:54:19 -0600

    added search bar

--- public/app/views/home/homeCtrl.js
+++ public/app/views/home/homeCtrl.js
@@ -2,4 +2,6 @@ var app = angular.module('treasureHunters');
 
 app.controller('homeCtrl', function($scope, listings) {
   $scope.theListings = listings;
+  $scope.active = true;
+  $scope.active1 = true;
 });
