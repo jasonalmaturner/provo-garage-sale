e626a5caee714dad608de53adf8e8c94f277af42
Justin Regan <justin.regan@gmail.com>
2015-08-11 11:04:46 -0600

    finished location and distance search

--- public/app/views/home/homeCtrl.js
+++ public/app/views/home/homeCtrl.js
@@ -8,17 +8,17 @@ app.controller('homeCtrl', function($scope, listings, mainService) {
   	$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 12 };
 
                         // Search by Distance
-    $scope.distances = ["5 miles", "15 miles", "25 miles"];
+    $scope.distances = ["5", "15", "25"];
 
                         // Search by location
 
-  	$scope.submitLocSearch = function(location){
+  	$scope.submitLocSearch = function(location, distance){
     	console.log('hitmainctrl', location, $scope.locationSearch);
     	mainService.geocode(location).then(function(res) {
     		console.log('geocodeinmain', res);
     		$scope.map.center.latitude = res.data.latitude;
     		$scope.map.center.longitude = res.data.longitude;
-    		mainService.getListings([res.data.longitude, res.data.latitude]).then(function(resp){
+    		mainService.getListings([res.data.longitude, res.data.latitude], distance).then(function(resp){
     			console.log("newlistingsmainctrl", resp);
             	$scope.theListings = resp;
             	console.log("thelistingsafter", $scope.theListings)
